e9496cea54789b883fc20b2fc3ca5ec4e52158df
Katya Popova <hadron@yandex.ru>
2020-07-20 15:56:48 +0300

    add response

--- app/src/main/java/ru/hadron/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/ru/hadron/newsapp/ui/NewsActivity.kt
@@ -2,16 +2,26 @@ package ru.hadron.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_news.*
 import ru.hadron.newsapp.R
+import ru.hadron.newsapp.db.ArticleDatabase
+import ru.hadron.newsapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
         //connect bottom navigation view with navigation component
         bottomNavigationView.setupWithNavController(newsNavGraphHostFragment.findNavController())
     }
--- app/src/main/java/ru/hadron/newsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/ru/hadron/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -1,7 +1,63 @@
 package ru.hadron.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 import ru.hadron.newsapp.R
+import ru.hadron.newsapp.adapters.NewsAdapter
+import ru.hadron.newsapp.ui.NewsActivity
+import ru.hadron.newsapp.ui.NewsViewModel
+import ru.hadron.newsapp.util.Resource
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG: String = "Breaking News Fragment"
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+
+        setupRecycleView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "error: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun setupRecycleView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
 }
